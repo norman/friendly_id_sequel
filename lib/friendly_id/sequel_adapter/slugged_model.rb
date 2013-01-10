129cca947645c0d305c35e44784a67d7d1316e5d
Jon de Andres <jon@wuaki.tv>
2013-01-10 18:46:17 +0100

    Call `super` on #validate method for SimpleModel and SluggedModel

--- lib/friendly_id/sequel_adapter/slugged_model.rb
+++ lib/friendly_id/sequel_adapter/slugged_model.rb
@@ -62,9 +62,11 @@ module FriendlyId
         build_slug if new_slug_needed?
         method = friendly_id_config.method
       rescue FriendlyId::BlankError
-        return errors.add(method, "can't be blank")
+        errors.add(method, "can't be blank")
       rescue FriendlyId::ReservedError
-        return errors.add(method, "is reserved")
+        errors.add(method, "is reserved")
+      ensure
+        super
       end
     end
   end
